8eaa6a8e2d1f6b9d6ac1797b5b2c2ca3c174b7e3
profeRonaldSena <ronald.barrera@cavirey.edu.co>
2023-05-18 09:59:22 -0500

    modified:   src/app/new/page.js

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,17 +1,23 @@
+'use client'
 import { useState } from 'react'
 
 const New = () => {
  const [task, setTask] = useState()
-
+ // funcion que captura los cambios en el formulario
  const handleChange = (e) => {
   setTask({
    ...task,
    [e.target.name]: [e.target.value],
   })
  }
+ //  funcion que captura cuando le doy al boton enviar
+ const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(task)
+ }
 
  return (
-  <form>
+  <form onSubmit={handleSubmit}>
    <input
     name='title'
     placeholder='escribe el titulo'
@@ -22,6 +28,7 @@ const New = () => {
     placeholder='escribe lo que tienes que hacer en la tarea'
     onChange={handleChange}
    />
+   <button>Save</button>
   </form>
  )
 }
