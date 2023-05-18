1e3b1fe0ade4dbb56d8d1e7680e635d7d8db8898
profeRonaldSena <ronald.barrera@cavirey.edu.co>
2023-05-18 09:12:55 -0500

    modified:   src/app/new/page.js

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,5 +1,29 @@
+import { useState } from 'react'
+
 const New = () => {
- return <h1>New</h1>
+ const [task, setTask] = useState()
+
+ const handleChange = (e) => {
+  setTask({
+   ...task,
+   [e.target.name]: [e.target.value],
+  })
+ }
+
+ return (
+  <form>
+   <input
+    name='title'
+    placeholder='escribe el titulo'
+    onChange={handleChange}
+   />
+   <textarea
+    name='description'
+    placeholder='escribe lo que tienes que hacer en la tarea'
+    onChange={handleChange}
+   />
+  </form>
+ )
 }
 
 export default New
